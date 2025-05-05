b3a6a94e014695fa5e815ebacb559a360fe973e8
Johnsoy.zhao <15520754767@163.com>
2025-05-05 19:23:51 +0800

    feat: Add method in wal.

--- storage/tinywal.go
+++ storage/tinywal.go
@@ -2,13 +2,21 @@ package storage
 
 import (
 	"SmartStashDB/tinywal"
+	lru "github.com/hashicorp/golang-lru/v2"
 	"sync"
 )
 
 type TinyWAL struct {
-	option        Options
-	mutex         sync.RWMutex
-	activeSegment *tinywal.SegmentFile
+	option           Options
+	mutex            sync.RWMutex
+	activeSegment    *tinywal.SegmentFile
+	immutableSegment map[SegmentFileId]*tinywal.SegmentFile
+	localCache       *lru.Cache[uint64, []byte]
+	byteWrite        uint64
+
+	pendingWritesLock sync.Mutex
+	pendingWrites     [][]byte
+	pendingWritesSize uint64
 }
 
 func (w *TinyWAL) close() error {
